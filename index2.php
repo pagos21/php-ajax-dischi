dc4e2a2fc2954a6df63e7dc05adcaea352deeb0a
pagos21 <paga@localhost.yeah>
2020-09-04 17:36:06 +0200

    v2.0 + BONUS

--- index2.php
+++ index2.php
@@ -36,7 +36,7 @@ function FunctionName($pseudoDB){
 			  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
 			  crossorigin="anonymous"></script>
 
-    <title>EX</title>
+    <title>EX2</title>
   </head>
   <body>
     <div class="headerBar">
--- script.js
+++ script.js
@@ -4,6 +4,22 @@ function init(){
   var compiled = Handlebars.compile(template);
   var target = $("#cont");
   getMyAPI(compiled, target);
+  $(document).on("click", "#btn", function(){
+      target.html("");
+    var val = $("#sel").val();
+    if (val == "def") {
+      getMyAPIfileterd(compiled, target, val);
+    } else if (val == "Rock") {
+      getMyAPIfileterd(compiled, target, val);
+    } else if (val == "Pop") {
+      getMyAPIfileterd(compiled, target, val);
+    } else if (val == "Jazz") {
+      getMyAPIfileterd(compiled, target, val);
+    } else if (val == "Metal") {
+      getMyAPIfileterd(compiled, target, val);
+    }
+  })
+
 };
 
 
@@ -36,6 +52,61 @@ function getMyAPI(compiled, target){
       console.log(error);
     }
   })
+}
+
+
+function getMyAPIfileterd(compiled, target, val){
+  $.ajax({
+    url: "index.php",
+    method: "GET",
+    data: {},
+    success: function(data, state){
+      var fromAPI = data['response'];
+      for (var i = 0; i < fromAPI.length; i++) {
+
+        var posterAPI = fromAPI[i].poster;
+        var titleAPI = fromAPI[i].title;
+        var authorAPI = fromAPI[i].author;
+        var genreAPI = fromAPI[i].genre;
+        var yearAPI = fromAPI[i].year;
+
+        if (val == genreAPI) {
+
+          var discs2html = compiled({ cover:posterAPI,
+            title:titleAPI,
+            author:authorAPI,
+            genre:genreAPI,
+            year:yearAPI
+
+          })
+
+          target.append(discs2html);
+        }
+        else if (val == "def") {
+          var discs2html = compiled({ cover:posterAPI,
+            title:titleAPI,
+            author:authorAPI,
+            genre:genreAPI,
+            year:yearAPI
+
+          })
+
+          target.append(discs2html);
+        }
+
+
+      } //for
+
+    },
+    error: function(error){
+      console.log(error);
+    }
+  })
+}
+
+
+function takeval(compiled, target){
+
 }
 
 $(document).ready(init);
